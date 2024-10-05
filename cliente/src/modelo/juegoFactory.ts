3aa919646e7c4ebcd37bf8574b3e2b6010b25ea3
Luis Waldman <luiswaldman@gmail.com>
2024-10-05 12:40:34 -0300

    Paso agregar imagenes a graficojuego !

--- cliente/src/modelo/juegoFactory.ts
+++ cliente/src/modelo/juegoFactory.ts
@@ -1,44 +1,30 @@
 import { graficoJuego } from './graficoJuego';
-import { Imagen } from './imagen';
-import { Animacion } from './animacion';
-import { Sonido } from './sonido';
-import { AnimacionEntidadGrafica } from './animacionentidadgrafica';
-
-import { Direcciones } from './Direcciones';
 
 export class JuegoFactory {
     juego_ejemplo1(): graficoJuego {
         const juego = new graficoJuego();
 
-        
         // Uso de la clase
-        juego.sonidos.push(new Sonido('cancion', Direcciones.obtenerConstante('cancion')!));
+        juego.AddSonido('cancion');
         
-        juego.imagenes.push(new Imagen('sky', Direcciones.obtenerConstante('sky')!));
-        juego.imagenes.push(new Imagen('ground', Direcciones.obtenerConstante('ground')!));
-        juego.imagenes.push(new Imagen('piso1', Direcciones.obtenerConstante('piso1')!));
-        juego.imagenes.push(new Imagen('piso2', Direcciones.obtenerConstante('piso2')!));
-        juego.imagenes.push(new Imagen('piso3', Direcciones.obtenerConstante('piso3')!));
-
+        juego.AddImagen('sky');
+        juego.AddImagen('ground');
+        juego.AddImagen('piso1');
+        juego.AddImagen('piso2');
+        juego.AddImagen('piso3');
+        juego.AddImagen('techo1');
+        juego.AddImagen('techo2');
+        juego.AddImagen('techo3');
+        juego.AddImagen('caja');
+        juego.AddImagen('star');
         
-        juego.imagenes.push(new Imagen('techo1', Direcciones.obtenerConstante('techo1')!));
-        juego.imagenes.push(new Imagen('techo2', Direcciones.obtenerConstante('techo2')!));
-        juego.imagenes.push(new Imagen('techo3', Direcciones.obtenerConstante('techo3')!));
-
+        juego.AddAnimacion('player_caminando', 70, 100);
+        juego.AddAnimacionEntidadGrafica('animacioncaminando', 'player_caminando', 0, 1, 7, -1);
+        juego.AddAnimacion('player_volando', 70, 100);
+        juego.AddAnimacionEntidadGrafica('animacionvolando', 'player_volando', 0, 1, 7, -1);
         
-        juego.imagenes.push(new Imagen('caja', Direcciones.obtenerConstante('caja')!));
-
-
-        juego.imagenes.push(new Imagen('star', Direcciones.obtenerConstante('star')!));
-        
-        juego.animaciones.push(new Animacion('player_caminando', Direcciones.obtenerConstante('player_caminando')!, 70, 100));
-        juego.animacionesendadgrafica.push(new AnimacionEntidadGrafica('animacioncaminando', 'player_caminando', 0, 1, 7, -1));
-        juego.animaciones.push(new Animacion('player_volando', Direcciones.obtenerConstante('player_volando')!, 70, 100));
-        juego.animacionesendadgrafica.push(new AnimacionEntidadGrafica('animacionvolando', 'player_volando', 0, 1, 7, -1));
-        
-        juego.animaciones.push(new Animacion('lazo', Direcciones.obtenerConstante('lazo')!, 20, 30));
-        juego.animacionesendadgrafica.push(new AnimacionEntidadGrafica('lazo', 'lazo', 0, 2, 4, -1));
-        
+        juego.AddAnimacion('lazo', 20, 30);
+        juego.AddAnimacionEntidadGrafica('lazo', 'lazo', 0, 2, 4, -1);
                 
 
         
