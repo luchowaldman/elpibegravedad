3dcb439cd710bb0f09f9c6dc71048b4d86d9c128
Luis Waldman <luiswaldman@gmail.com>
2024-10-06 17:39:24 -0300

    Controles

--- cliente/src/modelo/tiposplataforma/piso.ts
+++ cliente/src/modelo/tiposplataforma/piso.ts
@@ -14,11 +14,9 @@ export default class Piso extends Plataforma {
     dibujar(graficos: graficoJuego, index: number): void {
         
         const id = `ent_piso${index + 1}`;
-        console.log(id, "Dibuja piso", this.desdeX, this.desdeY);
 
         graficos.AdddEntidad(new EntidadGrafica(id, "piso1", this.desdeX, this.desdeY));
         for (let posX = this.desdeX + tamanio_figura; posX < this.hastaX - tamanio_figura; posX += tamanio_figura ) {
-            console.log(posX);
             graficos.AdddEntidad(new EntidadGrafica(id, "piso2", posX, this.desdeY));
         }
         graficos.AdddEntidad(new EntidadGrafica(id, "piso3", this.hastaX - 100, this.desdeY));
