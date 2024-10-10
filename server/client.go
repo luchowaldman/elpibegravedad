c2b5c41136cdb8ba5654f7f8455bc2b02c63ac5c
Franco Liberali <franco.liberali@gmail.com>
2024-10-09 22:15:57 -0300

    agregar chequeos de errores en on del cliente

--- server/client.go
+++ server/client.go
@@ -13,6 +13,32 @@ func manageClientConnection(clients []any, playersMutex *sync.Mutex, players *[]
 
 	log.Println("connection established. new client: ", newClientID)
 
+	err := newClient.On("changeGravity", func(datas ...any) {
+		log.Println("changeGravity event received")
+
+		// TODO: mutex for each player, not only for the list
+		playersMutex.Lock()
+		// players[newClient.Id()].InvertGravity()
+		(*players)[0].InvertGravity()
+		playersMutex.Unlock()
+	})
+	if err != nil {
+		log.Println("failed to register on changeGravity message")
+		newClient.Disconnect(true)
+	}
+
+	err = newClient.On("disconnect", func(...any) {
+		log.Println("client disconnected", newClient.Id())
+		// delete(players, newClientID)
+		playersMutex.Lock()
+		*players = []*Player{}
+		playersMutex.Unlock()
+	})
+	if err != nil {
+		log.Println("failed to register on disconnect message")
+		newClient.Disconnect(true)
+	}
+
 	playersMutex.Lock()
 	// players[newClientID] = Player{
 	// 	Socket: newClient,
@@ -33,22 +59,4 @@ func manageClientConnection(clients []any, playersMutex *sync.Mutex, players *[]
 		}
 	}
 	playersMutex.Unlock()
-
-	newClient.On("changeGravity", func(datas ...any) {
-		log.Println("changeGravity event received")
-
-		// TODO: mutex for each player, not only for the list
-		playersMutex.Lock()
-		// players[newClient.Id()].InvertGravity()
-		(*players)[0].InvertGravity()
-		playersMutex.Unlock()
-	})
-
-	newClient.On("disconnect", func(...any) {
-		log.Println("client disconnected", newClient.Id())
-		// delete(players, newClientID)
-		playersMutex.Lock()
-		*players = []*Player{}
-		playersMutex.Unlock()
-	})
 }
