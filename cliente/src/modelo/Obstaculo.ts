bb44dc2b06c0541e44a98377c2e3906e2ba38390
Luis Waldman <luiswaldman@gmail.com>
2024-10-13 14:29:57 -0300

    Agrego la propiedad largo y ancho

--- cliente/src/modelo/Obstaculo.ts
+++ cliente/src/modelo/Obstaculo.ts
@@ -1,27 +1,28 @@
 import { EntidadGrafica } from "./entidadgrafica";
 import { graficoJuego } from "./graficoJuego";
 
-  
   export class Obstaculo {
-    
-   
-    dibujar(graficos: graficoJuego) {
-      graficos.AdddEntidad(new EntidadGrafica(this.id, this.tipo, this.desdeX, this.desdeY));
-    }
     tipo: string;
     id: string;
     desdeX: number;
     desdeY: number;
-  
-    constructor(tipo: string, id: string, desdeX: number, desdeY: number) {
+    largo: number;
+    alto: number;
+
+    constructor(tipo: string, id: string, desdeX: number, desdeY: number, largo: number, alto: number) {
       this.id = id;
       this.tipo = tipo;
       this.desdeX = desdeX;
       this.desdeY = desdeY;
+      this.largo = largo;
+      this.alto = alto;
+    }
+
+    dibujar(graficos: graficoJuego) {
+      graficos.AdddEntidad(new EntidadGrafica(this.id, this.tipo, this.desdeX, this.desdeY, this.largo, this.alto));
     }
 
     cargarImagenes(graficos: graficoJuego): void {
       graficos.AddImagen(this.tipo);
+    }
   }
-  }
-  
