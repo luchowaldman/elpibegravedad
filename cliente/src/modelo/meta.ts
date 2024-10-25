6e38083421f87b7496bdb7f56c749c12b16ba684
Luis Waldman <luiswaldman@gmail.com>
2024-10-25 00:35:23 -0300

    Agrego la imagen de Meta Agrego la clase de Meta Dibujo la meta cuando dibuja el mapa

---
+++ cliente/src/modelo/meta.ts
@@ -0,0 +1,11 @@
+export class Meta {
+    x: number;
+    y: number;
+    alto: number;
+
+    constructor(x: number, y: number, alto: number) {
+        this.x = x;
+        this.y = y;
+        this.alto = alto;
+    }
+}
