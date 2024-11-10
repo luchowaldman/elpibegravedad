d7013866e3d897bd29811be318f5d69283953c1e
Luis Waldman <luiswaldman@gmail.com>
2024-11-10 16:48:15 -0300

    Agrego "Piso doble"

--- cliente/src/modelo/Direcciones.ts
+++ cliente/src/modelo/Direcciones.ts
@@ -7,11 +7,14 @@ export class Direcciones {
       piso1: '/img/piso_viro_ini.png',
       piso2: '/img/piso_viro_med.png',
       piso3: '/img/piso_viro_fin.png',
+      pisodoble1: '/img/piso_dob_ini.png',
+      pisodoble2: '/img/piso_dob_med.png',
+      pisodoble3: '/img/piso_dob_fin.png',
 
       techo1: '/img/techo_viro_ini.png',
       techo2: '/img/techo_viro_med.png',
       techo3: '/img/techo_viro_fin.png',
-
+ 
       caja: '/img/caja.png',
 
       star: 'https://labs.phaser.io/assets/sprites/star.png',
