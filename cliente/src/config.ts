b74693f3f88c44590de6e4ec01acbfa9a3d8eda2
luchowaldman <91391125+luchowaldman@users.noreply.github.com>
2024-11-27 21:00:11 -0300

    Agrego musica y corrigo link (#117)
    Co-authored-by: Luis Waldman <luiswaldman@gmail.com>

--- cliente/src/config.ts
+++ cliente/src/config.ts
@@ -5,6 +5,16 @@ document.addEventListener('DOMContentLoaded', () => {
 const usernameInput = document.getElementById('username') as HTMLInputElement;
 const serverInput = document.getElementById('server') as HTMLInputElement;
 const form = document.querySelector('form');
+const musicSelect = document.getElementById('music') as HTMLSelectElement;
+
+if (musicSelect) {
+    musicSelect.value = localStorage.getItem('music') || 'si';
+}
+
+musicSelect?.addEventListener('change', () => {
+    localStorage.setItem('music', musicSelect.value);
+});
+
 
 if (usernameInput && serverInput) {
     usernameInput.value = localStorage.getItem('username') || '';
@@ -19,6 +29,7 @@ form?.addEventListener('submit', (event) => {
     if (usernameInput && serverInput) {
         localStorage.setItem('username', usernameInput.value);
         localStorage.setItem('server', serverInput.value);
+        localStorage.setItem('music', musicSelect.value);
         window.location.href = 'index.html';
     }
 });
